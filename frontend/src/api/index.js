6fbe62be134b6f7f9803cc50296bd7983e7d8af4
mituyang <nostrnostr6@gmail.com>
2025-03-06 22:43:23 +0800

    Update index.js

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://api.searchsomething.top/api',
   headers: {
     'Content-Type': 'application/json'
   }
